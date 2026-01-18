ec7db9bddff5cbf54a3762c00702785ecd85245d
Selim Efe <uysalselimefe@gmail.com>
2026-01-18 22:59:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,22 @@
 
 int main()
 {
-           int sayilar[] = { 10,15,20,33,22,11,9,8,48,47,12,25,12,21 };
-    int i,sayi,x;
+    int sayilar[] = {10, 15, 20, 33, 22, 11, 9, 8, 48, 47, 12, 25, 12, 21};
+    int i, sayi, x;
 
     printf("sayi giriniz");
-    scanf("%d",&sayi);
+    scanf("%d", &sayi);
 
-    for(i=0;i<=13;i++){
-        if(sayi==sayilar[i]){
-            printf("%d. sırada",i+1);
-            x=1;
+    for (i = 0; i <= 13; i++)
+    {
+        if (sayi == sayilar[i])
+        {
+            printf("%d. sırada", i + 1);
+            x = 1;
         }
     }
-    if(x!=1){
+    if (x != 1)
+    {
         printf("Bu sayı dizide bulunmamaktadır");
     }
 
